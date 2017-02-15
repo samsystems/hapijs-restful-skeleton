56d0ad179fcd091f9c2fb6425876e0080bafb85f
yoan89 <yoan.uria8909@gmail.com>
2017-02-15 10:11:50 -0500

    include serializers
    (cherry picked from commit 789f9c7)

---
+++ src/core/helpers/querys.js
@@ -0,0 +1,33 @@
+'use strict';
+const _ = require('lodash');
+const internals = module.exports = {};
+
+internals.getSequilizeQueryOpts = (filters) => {
+    let limit = _.get(filters, 'limit', 25);
+    let page = _.get(filters, 'page', 1);
+    let opts = {
+        offset: limit * (page - 1),
+        limit: limit
+    };
+    let sort = _.get(filters, 'sort', false);
+    let fields = _.get(filters, 'fields', false);
+
+    if (sort) {
+        let direction = 'ASC';
+        if (sort[0] == '-') {
+            direction = 'DESC';
+            sort = sort.substring(1)
+        }
+        opts.order = [];
+        let sorts = sort.split(',');
+        _.each(sorts, (field) => {
+            opts.order.push([field, direction])
+        });
+    }
+
+    if (fields) {
+        opts.attributes = fields.split(',');
+    }
+
+    return opts;
+};
