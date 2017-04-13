545a10f74b0980627369bb5e1305de251c36a50f
Carlos Osiel Rojas Velázquez <osiel8809@gmail.com>
2017-04-13 14:26:02 -0400

    Add support for video call using twilio

--- src/plugins/twilio-integration/index.js
+++ src/plugins/twilio-integration/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
-let twilio = require('twilio');
+const twilio = require('twilio');
+const AccessToken = twilio.AccessToken;
+const BadRequestException = require('../../core/error/bad-request-exception');
 
 let twilioIntegration = {
     register: function (server, options, next) {
@@ -35,7 +37,7 @@ let twilioIntegration = {
 
         server.route({
             method: 'GET',
-            path: '/twilio/token',
+            path: '/twilio/token/call',
             handler: function(request, reply) {
                 let capability = new twilio.Capability(options.accountSid, options.authToken);
                 capability.allowClientOutgoing(options.twimlAppSid);
@@ -43,6 +45,34 @@ let twilioIntegration = {
                     token: capability.generate()
                 };
                 reply(token);
+            }
+        });
+        /**
+         * Return a correct token
+         * */
+        server.route({
+            method: 'GET',
+            path: '/twilio/token/video',
+            handler: function(request, reply) {
+                const {identity} = request.query;
+                if(identity) {
+                    let accessToken = new AccessToken(
+                        options.accountSid,
+                        options.twilioAPIKey,
+                        options.twilioAPIKeySecret
+                    );
+                    accessToken.identity = identity;
+                    let grant = new AccessToken.VideoGrant();
+                    grant.configurationProfileSid = options.applicationSid;
+                    accessToken.addGrant(grant);
+                    let token = {
+                        token: accessToken.toJwt()
+                    };
+                    reply(token);
+                }
+                else {
+                    throw new BadRequestException('The identity params is required');
+                }
             }
         });
         next();
