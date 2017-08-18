246127cffd26361f34df7995bd9d4be673571ba9
Carlos Osiel Rojas Velázquez <osiel8809@gmail.com>
2017-08-18 10:13:18 -0400

    Remove duplicated core folder

--- src/core/core/error/forbidden-exception.js
+++
@@ -1,8 +0,0 @@
-const BaseException = require('./base');
-
-module.exports = class ForbiddenException extends BaseException {
-
-    constructor(msg, code) {
-        super('Access Denied', msg, code ? code : 403);
-    }
-};
--- src/core/core/error/validation-exception.js
+++
@@ -1,8 +0,0 @@
-const BaseException = require('./base');
-
-module.exports = class ValidationException extends BaseException {
-
-    constructor(msg, code) {
-        super('Unprocessable Entity', msg, code ? code : 422);
-    }
-};
